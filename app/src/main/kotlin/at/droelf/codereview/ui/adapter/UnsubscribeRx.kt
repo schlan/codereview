95a2c5647be66d768ae23b78bb67f4c8bbeac0b0
Sebastian Chlan <schlan@zendesk.com>
2016-01-25 20:35:05 +0000

    things and stuff

---
+++ app/src/main/kotlin/at/droelf/codereview/ui/adapter/UnsubscribeRx.kt
@@ -0,0 +1,8 @@
+package at.droelf.codereview.ui.adapter
+
+/**
+ * Created by schlan on 25/01/2016.
+ */
+public interface UnsubscribeRx{
+    fun unsubscribeRx()
+}
---
+++ app/src/main/kotlin/at/droelf/codereview/ui/view/PullRequestFileView.kt
@@ -0,0 +1,113 @@
+package at.droelf.codereview.ui.view
+
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.support.design.widget.Snackbar
+import android.support.v4.app.FragmentManager
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import at.droelf.codereview.R
+import at.droelf.codereview.model.GithubModel
+import at.droelf.codereview.ui.adapter.NotificationFragmentAdapter
+import at.droelf.codereview.ui.adapter.PullRequestCommentsAdapter
+import at.droelf.codereview.ui.adapter.UnsubscribeRx
+import at.droelf.codereview.ui.fragment.NotificationFragmentController
+import at.droelf.codereview.ui.fragment.StartFragmentController
+import butterknife.Bind
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
+import rx.Observable
+import rx.Subscription
+
+class PullRequestFileView(context: Context, val pr: GithubModel.PullRequestDetail, val fm: FragmentManager, val controller: StartFragmentController): FrameLayout(context), UnsubscribeRx {
+
+    val list: ListView
+    val swipeToRefresh: SwipeRefreshLayout
+    var listAdapter: Adapter? = null
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_pr_files, this, true)
+        swipeToRefresh = findViewById(R.id.pr_files_swipe_to_refresh) as SwipeRefreshLayout
+        list = findViewById(R.id.pr_files_list) as ListView
+        init()
+    }
+
+    fun init() {
+        swipeToRefresh.isEnabled = false
+        swipeToRefresh.setColorSchemeResources(R.color.colorAccent)
+        swipeToRefresh.setOnRefreshListener {
+            swipeToRefresh.isRefreshing = false
+        }
+
+//        list.addItemDecoration(DividerItemDecoration(context, resources.getDimensionPixelOffset(R.dimen.row_notification_pull_request_divider_padding_left)))
+//        list.layoutManager = LinearLayoutManager(context)
+//        list.itemAnimator = SlideInUpAnimator()
+
+
+        val owner = pr.base.repo.owner.login
+        val repo = pr.base.repo.name
+        val number = pr.number
+        val comments = controller.comments(owner, repo, number)
+
+        listAdapter = Adapter(controller.prfiles(owner, repo, number), swipeToRefresh) //FIXME to recycler
+        list.adapter = listAdapter
+        list.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val file = (view.tag as GithubModel.PullRequestFile)
+            controller.showFile(fm, file.contentsUrl, file.patch, file.filename, owner, repo, number)
+        }
+    }
+
+    override fun unsubscribeRx() {
+        listAdapter?.unsubscribeRx()
+        listAdapter = null
+    }
+
+
+    class Adapter(prfiles: Observable<List<GithubModel.PullRequestFile>>, swipeToRefresh: SwipeRefreshLayout) : BaseAdapter(), UnsubscribeRx {
+
+        var files: MutableList<GithubModel.PullRequestFile> = arrayListOf()
+        var subscription: Subscription?
+
+        init {
+            swipeToRefresh.post({ swipeToRefresh.isRefreshing = true })
+            subscription = prfiles.subscribe ({ data ->
+                files.addAll(data)
+                notifyDataSetChanged()
+            },{
+                Snackbar.make(swipeToRefresh, "Error: ${it.message}", Snackbar.LENGTH_LONG).show()
+                swipeToRefresh.post({ swipeToRefresh.isRefreshing = false })
+            },{
+                swipeToRefresh.post({ swipeToRefresh.isRefreshing = false })
+            })
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+            val view = convertView ?: LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+            (view.findViewById(android.R.id.text1) as TextView).text = files[position].filename
+            view.background = ColorDrawable(ContextCompat.getColor(parent.context, R.color.bg_white))
+            view.tag = files[position]
+            return view
+        }
+
+        override fun getItem(position: Int): Any? {
+            return files[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
+
+        override fun getCount(): Int {
+            return files.size
+        }
+
+        override fun unsubscribeRx() {
+            subscription?.unsubscribe()
+        }
+    }
+}
